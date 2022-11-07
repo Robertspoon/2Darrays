b4ff159972da11359a88cf7e4b90b24e7e468e8a
Robertspoon <robertthemcjug@gmail.com>
2022-11-07 11:17:34 -0400

    adawdwda

--- 2D arrays/Program.cs
+++ 2D arrays/Program.cs
@@ -11,11 +11,14 @@ namespace _2D_arrays
         static void Main(string[] args)
         {
             int[,] Array2D = new int[4, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+            string[,] Array2D2 = new string[2, 2] { { "Goodbye", "Hello" }, { "No", "Yes" } };
 
             Console.WriteLine(Array2D[0, 2]);
             Console.WriteLine(Array2D[3, 2]);
             Console.WriteLine(Array2D[2, 0]);
             Console.WriteLine(Array2D[1, 2]);
+            Console.WriteLine(Array2D2[0, 1]);
+            Console.WriteLine(Array2D2[1, 0]);
 
 
             Console.ReadKey(true);
